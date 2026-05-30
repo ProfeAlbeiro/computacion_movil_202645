7154220bc79f0ae59d2de80bc49123fdc3294f61
ProfeAlbeiro <profealbeiro2020@gmail.com>
2026-05-30 16:07:23 -0500

    Estructura de carpetas y archivos

--- frontend_mob/App.tsx
+++ frontend_mob/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundossss!</Text>
+      <Text>Hola Mundosssss!</Text>
       <StatusBar style="auto" />
     </View>
   );
